fdc42d15a147aca3a30165744959133c3b2ac14d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2026-01-22 12:59:55 +0530

    Returning multiple tags inside a div

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import {Welcome} from './Welcome'
 import {Button} from './Button'
 import { Hello, HelloWithoutJSX} from './Hello'
+import { UserProfile } from './UserProfile'
 
 function App() {
   
   return (
     <div>
+      <UserProfile />
       <Hello /> 
       <HelloWithoutJSX />
       <Welcome />
---
+++ src/UserProfile.jsx
@@ -0,0 +1,8 @@
+export const UserProfile = () => {
+    return (
+        <div>
+            <h1>Convolution React Course</h1>
+            <p>Author: Umesh</p>
+        </div>
+    )
+}
