7bce53d1627e9af6a0be23753bdc8a99608ff46e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2026-01-22 13:04:43 +0530

    <> instead of react.fragment

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,9 +1,8 @@
-import React from "react"
 export const UserProfile = () => {
     return (
-        <React.Fragment>
-            <h1>Convolution React Course</h1>
-            <p>Author: Umesh</p>
-        </React.Fragment>
+        <>
+        <h1>Convolution React Course</h1>
+        <p>Author: Umesh</p>
+        </>
     )
 }
