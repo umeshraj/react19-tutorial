c857c2f0a49b1d3ecc61b31863bb080f4a180c35
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2026-01-23 21:07:04 +0530

    Passing fn to useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 
 export const Counter = () => {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
   //   let count = 0;
+
+  const [count, setCount] = useState(() => {
+    console.log("Initial state fn called");
+    return 0;
+  });
   console.log("Counter component rendered with count:", count);
   const handleClick = () => {
     // count += 1;
