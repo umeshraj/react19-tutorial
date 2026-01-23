e46f77c548e5c9b95150abe9b4e86867b8000f3c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2026-01-23 17:33:43 +0530

    Style w/ .css

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,16 +1,5 @@
+import "./Alert.css";
+
 export const Alert = ({ children, type = "success" }) => {
-  return (
-    <div
-      style={{
-        backgroundColor: type === "success" ? "#d4edda" : "#f8d7da",
-        color: "#155724",
-        padding: "10px",
-        borderRadius: "5px",
-        border: "1px solid #c3e6cb",
-        marginBottom: "15px",
-      }}
-    >
-      {children}{" "}
-    </div>
-  );
+  return <div className={`alert ${type}`}>{children} </div>;
 };
