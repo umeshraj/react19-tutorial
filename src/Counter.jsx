0eec25ac809d6443e28a794081b74672f9d91c7c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2026-01-23 21:04:56 +0530

    Use of useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 export const Counter = () => {
-  let count = 0;
-  console.log("Counter component rendered");
+  const [count, setCount] = useState(0);
+  //   let count = 0;
+  console.log("Counter component rendered with count:", count);
   const handleClick = () => {
-    count += 1;
-    console.log(count);
+    // count += 1;
+    setCount(count + 1);
+    // console.log(count);
   };
   return <button onClick={handleClick}>Count:{count}</button>;
 };
